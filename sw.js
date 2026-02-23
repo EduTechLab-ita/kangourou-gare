3ff5f531e0c9e9be793cbf43a3c8fdaeef672a92
EduTechLab <edutechlab.ita@gmail.com>
2026-02-23 18:44:42 +0100

    Bump cache v2.3.1 per invalidare immagini q03/q08/q23 aggiornate

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker per PWA Kangourou Trainer
-const CACHE_NAME = 'kangourou-trainer-v2.3.0';
+const CACHE_NAME = 'kangourou-trainer-v2.3.1';
 const urlsToCache = [
   './',
   './index.html',
