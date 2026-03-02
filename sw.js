ddce631ff9d8e01f243ca26e3312645ee29a2424
EduTechLab <edutechlab.ita@gmail.com>
2026-03-02 22:42:34 +0100

    v2.21.0: Kangourou 2007-2012 - screenshot completo domande
    Applicate le stesse migliorie di Kangourou 2013:
    - Screenshot intera domanda (testo + figure + opzioni)
    - Pulsanti solo lettera A-E
    - Fix pagine soluzioni (pattern 'Risposta')
    - Fix domande senza spazio dopo punto (es. '11.Un lato...')

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker per PWA Kangourou Trainer
-const CACHE_NAME = 'kangourou-trainer-v2.20.0';
+const CACHE_NAME = 'kangourou-trainer-v2.21.0';
 const urlsToCache = [
   './',
   './index.html',
